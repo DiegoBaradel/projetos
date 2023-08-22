7591615a9d037299b999391c0e4f7fe0e3638a30
DiegoBaradel <diego_baradel@hotmail.com>
2023-08-22 19:25:32 -0300

    portifolio

--- projeto-potifolio/src/js/index.js
+++ projeto-potifolio/src/js/index.js
@@ -1,6 +1,35 @@
 const botaoTema = document.querySelector('#btn-tema')
 const botaoSlide = [...document.querySelectorAll('.botao-slide')]
 
+const rotinaResetIntervalo = ()=>{
+    clearInterval(intervalo)
+    intervalo = setInterval(rotinaIntervalo,5000)
+}
+const rotinaIntervalo = ()=>{
+    const largura = document.querySelector('#slide-projetos-eventos').clientWidth
+    let botaoAtivo
+    let indexBotaoAtivo
+
+    botaoSlide.forEach((botao, index)=>{
+        if(botao.classList.contains('botao-ativo')){
+            botaoAtivo = botao
+            indexBotaoAtivo = index
+        }
+    })
+    botaoAtivo.classList.remove('botao-ativo')
+
+    if(botaoAtivo.nextElementSibling){
+        botaoAtivo.nextElementSibling.classList.add('botao-ativo')
+        document.querySelector('#imagem-slide').style.left = `-${largura*indexBotaoAtivo+largura}px`
+    }
+    else{
+        botaoAtivo.parentElement.firstElementChild.classList.add('botao-ativo')
+        document.querySelector('#imagem-slide').style.left = '-0px'
+    }
+}
+
+let intervalo = setInterval(rotinaIntervalo,5000)
+
 botaoTema.addEventListener('click',(evt)=>{
     const corpo = document.querySelector('body')
     corpo.classList.toggle('tema')
@@ -22,6 +51,8 @@ botaoTema.addEventListener('click',(evt)=>{
 botaoSlide.forEach((botao,index)=>{
     botao.addEventListener('click',(evt)=>{
 
+        rotinaResetIntervalo()
+
         botaoSlide.forEach(removerClass=>{
             removerClass.classList.remove('botao-ativo')
         })
@@ -32,3 +63,15 @@ botaoSlide.forEach((botao,index)=>{
         document.querySelector('#imagem-slide').style.left = `-${largura*index}px`
     })
 })
+
+window.addEventListener('resize',(evento)=>{
+
+    rotinaResetIntervalo()
+    
+    botaoSlide.forEach(botao=>{
+        botao.classList.remove('botao-ativo')
+    })
+    botaoSlide[0].classList.add('botao-ativo')
+    document.querySelector('#imagem-slide').style.left = '-0px'
+})
+
