a776170a12bc4dc18299d9e6c0c1caee19e95fe1
DiegoBaradel <diego_baradel@hotmail.com>
2023-10-04 20:37:16 -0300

    melhorando codigo

--- projeto-one-piece/src/js/index.js
+++ projeto-one-piece/src/js/index.js
@@ -3,14 +3,20 @@ const listaPersonagens= document.querySelectorAll('.personagem')
 
 listaBotoes.forEach((botao,index)=>{
     botao.addEventListener('click',()=>{
-        const botaoSelecionado = document.querySelector('.botao.selecionado');
-        botaoSelecionado.classList.remove('selecionado');
 
-        botao.classList.add('selecionado');
-
-        const personagemSelecionado =document.querySelector('.personagem.selecionado');
-        personagemSelecionado.classList.remove('selecionado');
-
+        desselecionarBotao();
+        desselecionarPersonagem();
+        botao.classList.add('selecionado');    
         listaPersonagens[index].classList.add('selecionado');
-    })
-})
+        
+    });
+});
+
+function desselecionarPersonagem(){
+    const personagemSelecionado =document.querySelector('.personagem.selecionado');
+        personagemSelecionado.classList.remove('selecionado');
+}
+function desselecionarBotao(){
+    const botaoSelecionado = document.querySelector('.botao.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
